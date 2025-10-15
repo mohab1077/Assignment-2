dec3680b2bf524826785fbf8afada5a4976e1b3f
mohabel <mohab1077@gmail.com>
2025-10-15 21:50:50 +0300

    added greddy search into oop_code

--- oop_code.cpp
+++ oop_code.cpp
@@ -16,11 +16,6 @@ struct Task
     string name;
     int dep; /// this is option (only used on a*)
 };
-struct path
-{
-    int val;
-    string path;
-};
 
 struct Cmp
 {
@@ -56,39 +51,215 @@ public:
         return -1;
     }
 
-    string move_up(string a)
+    int h1(string start, string goal)
     {
+        int h = 0;
+        for (int i = 0; i <= 8; i++)
+        {
+            if (start[i] == '0')
+            {
+                continue;
+            }
+            if (start[i] != goal[i])
+            {
+                h++;
+            }
+        }
+        return h;
     }
-    string move_down(string a)
+    int searchindex(string s, char find)
     {
+        for (int i = 0; i < (int)s.length(); i++)
+        {
+            if (s[i] == find)
+                return i;
+        }
+        return -1;
     }
-    string move_left(string a)
+    int h2(string start, string goal)
     {
-    }
-    string move_right(string a)
-    {
+        int h = 0;
+        for (int i = 0; i <= 8; i++)
+        {
+            if (start[i] == '0')
+            {
+                continue;
+            }
+
+            int index = i;
+            int row = index / 3;
+            int col = index % 3;
+            /////////////////
+            int index2 = searchindex(goal, start[i]);
+            int row2 = index2 / 3;
+            int col2 = index2 % 3;
+            if (row != row2)
+            {
+                h++;
+            }
+            if (col != col2)
+            {
+                h++;
+            }
+        }
+        return h;
     }
 
-    queue<path> expand_node(string expanded)
+    string move_up(string expanded, int idx)
     {
-        queue<path> actions;
+        swap(expanded[idx], expanded[idx + 3]);
+    }
+    string move_down(string expanded, int idx)
+    {
+        swap(expanded[idx], expanded[idx - 3]);
+    }
+    string move_left(string expanded, int idx)
+    {
+        swap(expanded[idx], expanded[idx + 1]);
+    }
+    string move_right(string expanded, int idx)
+    {
+        swap(expanded[idx], expanded[idx - 1]);
+    }
+
+    queue<string> expand_node(string expanded)
+    {
+        queue<string> actions;
         int index = searchzero(expanded);
         int row = index / 3;
         int col = index % 3;
 
         if (row > 0)
-            actions.push({index - 3, "down"});
+            actions.push({"down"});
         if (row < 2)
-            actions.push({index + 3, "up"});
+            actions.push({"up"});
         if (col > 0)
-            actions.push({index - 1, "right"});
+            actions.push({"right"});
         if (col < 2)
-            actions.push({index + 1, "left"});
+            actions.push({"left"});
         return actions;
+    }
+
+    void printing_result(string start, string goal, int nodes_frontire, int nodes_exp)
+    {
+        vector<string> path;
+        vector<string> buzz;
+        string cur = goal;
+        while (cur != start)
+        {
+            path.push_back(movee[cur]);
+            buzz.push_back(cur);
+            cur = parent[cur];
+        }
+        buzz.push_back(start);
+        reverse(path.begin(), path.end());
+        reverse(buzz.begin(), buzz.end());
+
+        cout << "States: " << endl;
+        for (auto &st : buzz)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                cout << st[i] << " ";
+                if ((i + 1) % 3 == 0)
+                    cout << endl;
+            }
+            cout << "------" << endl;
+        }
+
+        cout << "Path: ";
+        for (auto &m : path)
+            cout << m << " ";
+        cout << endl;
+
+        cout << "Steps (Cost): " << path.size() << endl;
+        cout << "Number of Nodes Entering the Frontier : " << nodes_frontire << endl;
+        cout << "Number of Nodes Expanded from the Frontier : " << nodes_exp << endl;
+        return;
     }
 
     void greddy_search(string start, string goal, int main_h)
     {
+        priority_queue<Task, vector<Task>, Cmp> frontire;
+        unordered_set<string> checkfrontire_exp; // ignore fronitrer and explored;
+        int nodes_frontire = 0;
+        int nodes_exp = 0;
+        int h;
+        if (main_h == 1)
+        {
+            h = h1(start, goal);
+        }
+        if (main_h == 2)
+        {
+            h = h2(start, goal);
+        }
+        frontire.push({h, start});
+        nodes_frontire++;
+        checkfrontire_exp.insert(start);
+        parent[start] = "";
+        movee[start] = "";
+        while (true)
+        {
+            if (frontire.empty())
+            {
+                cout << "Goal not found!\n";
+                return;
+            }
+
+            string expanded = frontire.top().name;
+            nodes_exp++;
+            frontire.pop();
+            if (expanded == goal)
+            {
+                printing_result(start, goal, nodes_frontire, nodes_exp);
+                break;
+                /// will complete later;
+            }
+
+            int index = searchzero(expanded);
+            queue<string> actions = expand_node(expanded);
+
+            while (!actions.empty())
+            {
+                string newbuzz;
+                if (actions.front() == "down")
+                {
+                    newbuzz = move_down(expanded, index);
+                }
+                else if (actions.front() == "up")
+                {
+                    newbuzz = move_up(expanded, index);
+                }
+                else if (actions.front() == "right")
+                {
+                    newbuzz = move_right(expanded, index);
+                }
+                else
+                {
+                    newbuzz = move_left(expanded, index);
+                }
+                string mv = actions.front();
+                actions.pop();
+                if (checkfrontire_exp.find(newbuzz) != checkfrontire_exp.end())
+                {
+                    continue;
+                }
+                parent[newbuzz] = expanded;
+                movee[newbuzz] = mv;
+                int h;
+                if (main_h == 1)
+                {
+                    h = h1(newbuzz, goal);
+                }
+                if (main_h == 2)
+                {
+                    h = h2(newbuzz, goal);
+                }
+                frontire.push({h, newbuzz});
+                nodes_frontire++;
+                checkfrontire_exp.insert(newbuzz);
+            }
+        }
     }
 
     void a_star(string start, string goal, int main_h)
